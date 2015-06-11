d65cc0ed46fc606b8ed916ec8ccb7e020d8da767
Chris O'Donnell <codonnell822@gmail.com>
2015-06-11 13:32:15 -0400

    refactor plugin, allow data attributes to override options

--- src/jquery.data-remote.js
+++ src/jquery.data-remote.js
@@ -1,50 +1,33 @@
 /**
- * jQuery dataRemote Plugin v0.2.0
+ * jQuery dataRemote Plugin
  * http://github.com/codonnell822/jquery-dataremote
- * Copyright 2014, Chris O'Donnell
+ * Copyright 2014-2015, Chris O'Donnell
  *
  * Plugin to help make handling ajax requests easier.
  * Name inspired by Ruby on Rails
  */
 ;(function($, window, undefined) {
+  "use strict";
 
   $.fn.dataRemote = function(opts) {
-    "use strict";
-
-    /**
-     * Default Settings
-     *
-     * @see https://api.jquery.com/jQuery.ajax/
-     */
+    // Default Settings
     var defaults = {
       url: null, // request url
       data: {}, // request data
       debug: false,
-      eventType: 'load', // js event type to fire data request on (load, click, submit, mouseover, etc.)
+      eventType: 'load', // js event type to fire data request on (load, click, submit, mouseover, custom, etc.)
       dataType: 'json', // data type that's expected in response from your request
       type: 'GET', // type of request (currently only supports GET)
-      target: null, // selector of the element where you want your response injected
+      target: '', // selector of the element where you want your response injected
       handlebars: false, // use handlebars to render the response?
-      template: null, // handlebar templates' selector. by default it will look inside target
+      template: '', // handlebar template selector. by default it will look inside target
+      placement: 'html', // where to inject response relative to target (jquery DOM insertion methods <html|append|prepend|before|after>)
       loaderImg: null, // target selector for data response
-      oneAndDone: true, // after the first time running, wont run again
-      append: false, // append result to target element? default is to replace content
+      oneAndDone: true, // whether to remove the event binding after the first time running
       success: successCallback, // gets passed 3 parameters ($target, options, response)
       error: errorCallback, // gets passed 4 parameters ($target, options, response, error)
-
-      /**
-       * Callback that fires after the request is made (on success OR error)
-       *
-       * @param $target {jQuery} jQuery object containing target element(s)
-       */
-      complete: function($target) {},
-
-      /**
-       * Callback that fires before the request is made
-       *
-       * @param $target {jQuery} jQuery object containing target element(s)
-       */
-      before: function($target) {}
+      complete: function($target) {}, // callback fires after the request is made (on success OR error)
+      before: function($target) {} // callback fires directly before the request is made
     };
 
     // Extend our default options with those provided when instantiating
@@ -53,8 +36,9 @@
     /**
      * Default success callback for ajax requests.
      *
-     * Handles hiding of loader image, debugging (if setting is true),
-     * and templating request response.
+     * Handles hiding of loader image, debugging (if option is set),
+     * and handling the ajax requests' response (levering handlebars if option
+     * is set or handlebars template is found inside the target element).
      *
      * @param  {jQuery} $target  jquery object containing the target element for the ajax response
      * @param  {object} options  plugin options set during instantiation
@@ -73,7 +57,7 @@
       }
 
       // Use handlebars if the option is set to true *or* if a handlebars template is found
-      if (options.handlebars === true || $target.find('[type="text/x-handlebars-template"]').length) {
+      if (options.handlebars !== false || $target.find('[type="text/x-handlebars-template"]').length) {
         $template = options.template ? $(options.template) : $target.find('[type="text/x-handlebars-template"]');
         source = $template.html();
         template = Handlebars.compile(source);
@@ -85,8 +69,12 @@
       // use proper jQuery method based on append option
       // if append == true, append results to target element,
       // or else replace inner html with results
-      var method = options.append === true ? 'append' : 'html';
-      $target[method](html);
+      var validPlacements = ['html', 'append', 'prepend', 'after', 'before'];
+      var placementIsValid = validPlacements.indexOf(options.placement) !== -1;
+      if (!placementIsValid) {
+        return;
+      }
+      $target[options.placement](html);
     }
 
     /**
@@ -116,7 +104,6 @@
      *    @param {object} context  the value of `this` provided for the callbacks
      *    @param {object} options  options for current request
      *    @param {object} $target  the jquery target element to inject response
-     *    @param {object} dataType request data type (see $.ajax documentation)
      * }
      * @return {void}
      */
@@ -124,7 +111,6 @@
       var $context = settings.context;
       var $target = settings.target;
       var options = settings.options;
-      var dataType = settings.dataType;
 
       // display loader image while retrieving content
       if (options.loaderImg) {
@@ -139,7 +125,7 @@
         url: url,
         data: options.data,
         type: options.type,
-        dataType: dataType,
+        dataType: options.dataType,
         cache: true,
         success: function(response) {
           options.success.call($context, $target, options, response);
@@ -154,43 +140,54 @@
 
     return this.each(function(idx, element) {
       var $element = $(element);
-      var dataType = $element.data('type') || options.dataType; // response data type
-      var eventType = $element.data('event') || options.eventType; // event type to make the request on
-      var target = $element.data('target') || options.target; // target selector for handling the response
-      var $target = target ? $(target) : $element; // no target given, default to actual element
-      var url = $element.data('url') || options.url; // remote url to make the GET or POST request to
+
+      // set a local scope options variable for each element and
+      // override options from data attributes of said element if set
+      var _options = options;
+      _options.dataType = $element.data('response-type') || options.dataType;
+      _options.eventType = $element.data('event-type') || options.eventType;
+      _options.type = $element.data('type') || options.type;
+      _options.url = $element.data('url') || options.url;
+      _options.data = $element.data('data') ? JSON.parse($element.data('data')) : options.data;
+      _options.oneAndDone = !!$element.data('one-and-done') || options.oneAndDone;
+      _options.handlebars = !!$element.data('handlebars') || options.handlebars;
+      _options.template = $element.data('template') || options.template;
+      _options.placement = $element.data('response-placement') || options.placement;
+      _options.debug = !!$element.data('debug') || options.debug;
+      _options.target = $element.data('target') || options.target;
+
+      // if no target selector is given, default to actual element
+      var $target = _options.target ? $(_options.target) : $element;
 
       // if event type is 'load', execute the request immediately otherwise,
       // execute ajax request on specified type (click, submit, mouseover, etc.)
-      if (eventType === 'load') {
-        options.before.call($element, $target);
+      if (_options.eventType === 'load') {
+        _options.before.call($element, $target);
 
         // execute ajax request immediately
-        get(url, {
+        get(_options.url, {
           context: $element,
-          options: options,
           target: $target,
-          dataType: dataType
+          options: _options,
         });
       } else {
         // use proper jQuery method based on oneAndDone option
-        var method = options.oneAndDone === true ? 'one' : 'on';
+        var method = _options.oneAndDone === true ? 'one' : 'on';
 
         // bind to specific event type
-        $element[method](eventType, function(e) {
+        $element[method](_options.eventType, function(e) {
           e.preventDefault();
 
           // execute before request callback
-          options.before.call($element, $target);
+          _options.before.call($element, $target);
 
-          get(url, {
+          get(_options.url, {
             context: $element,
-            options: options,
             target: $target,
-            dataType: dataType
+            options: _options,
           });
         });
-      } // end if else (eventType)
+      } // end if else (_options.eventType)
     }); // end this.each
   }; // end $.fn.dataRemote
 
