467c9b5321adf9d7d04aacd2a0dd256e7dd8b242
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2015-06-22 11:46:24 -0400

    added debounce

--- src/jquery.data-remote.js
+++ src/jquery.data-remote.js
@@ -96,6 +96,25 @@
     }
 
     /**
+     * Private helper method for debounce
+     *
+     */
+    function debounce(func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+
+    /**
      * Private helper method for making the ajax request
      *
      * @param {object} url       request url
@@ -177,7 +196,7 @@
         var method = _options.oneAndDone ? 'one' : 'on';
 
         // bind to specific event type
-        $element[method](_options.eventType, function(evt) {
+        $element[method](_options.eventType, debounce(function(evt) {
           evt.preventDefault();
 
           // if you're watching on keyup or change events, let's assume you want to
@@ -196,7 +215,7 @@
             target: $target,
             options: _options,
           });
-        });
+        }, 500);
       } // end if else (_options.eventType)
     }); // end this.each
   }; // end $.fn.dataRemote
